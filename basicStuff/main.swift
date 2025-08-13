e0854c7b8360e01afe998549d3b4a039724eddf6
tuanbeovnn <49017369+tuanbeovnn@users.noreply.github.com>
2025-08-13 13:26:32 +0300

    Day4
    Finished Day4

--- basicStuff/main.swift
+++ basicStuff/main.swift
@@ -7,5 +7,47 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Function")
+
+func add(a: Int, b: Int) -> Int {
+    return a + b
+}
+
+let sum = add(a: 3, b: 5)
+print(sum)
+
+
+func greetUser(name: String)
+{
+    print("Hello, \(name)")
+}
+greetUser(name: "Alice")
+
+//📌 Task 2:
+//Write a function isEven(number: Int) -> Bool that returns true if the number is even, otherwise false.
+
+func isEven(number: Int) -> Bool {
+    if number % 2 == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+let number = isEven(number: 10)
+
+print("Number is: \(number)")
+
+func welcomeMessage(name: String, city: String) {
+    print("Hello, \(name)! Welcome to \(city).")
+}
+
+welcomeMessage(name: "Tuan", city: "Hanoi")
+
+
+
+
+
+
+
 

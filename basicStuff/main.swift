6d8066be312b84cb8b26034c70b61275ac590975
tuanbeovnn <49017369+tuanbeovnn@users.noreply.github.com>
2025-08-14 10:08:38 +0300

    Day 7 finished

--- basicStuff/main.swift
+++ basicStuff/main.swift
@@ -7,5 +7,54 @@
 
 import Foundation
 
-print("Hello, World!")
+class Dog {
+    var name : String
+    var age: Int
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    func bark() {
+        print("\(name) say Woohoo!")
+    }
+}
+
+
+var dog1 = Dog(name: "Max", age: 5)
+var dog2 = Dog(name: "Daisy", age: 3)
+
+print(dog1.name)
+
+
+class Book {
+    var title: String
+    var author: String
+    
+    init(title: String, author: String) {
+        self.title = title
+        self.author = author
+    }
+    
+    func description() {
+        print("Book title: \(title), Author: \(author)")
+    }
+}
+
+var book1 = Book(title: "Book 1", author: "Messi")
+
+book1.description()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
